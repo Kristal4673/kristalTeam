da80af24edbc4b7dadd2dc69982db865e95343a8
Crystal Soto-Estrada <Csoto.lacombe@gmail.com>
2021-11-14 17:26:01 -0600

    updated the managers console file rendering

--- dist/lib/Manager.js
+++ dist/lib/Manager.js
@@ -1,19 +1,82 @@
 var inquirer = require("inquirer");
 inquirer
-  .prompt([
-      /* Pass your questions in here */
-      name: {{input}},
-      id: "1",
-      email: "crystal@gmail.com"
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-      {{input}}
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+    .prompt([
+        /* Passing questions for command-line prompt here in object */
+        {
+            name: "todo",
+            message: " What would you like to do?",
+            type: "input",
+        },
+        {
+            name: "dept",
+            message: "What is the name of the department",
+            type: "input",
+        },
+        {
+            name: "roles",
+            message: "What would you like to do?",
+            type: "input",
+        },
+        {
+            name: "cust",
+            message: "What is the name of the role?",
+            type: "input",
+        },
+        {
+            name: "#",
+            message: "What is the salary of the role?",
+            type: "input",
+        },
+        {
+            name: "belong",
+            message: "Which department does the role belong to?",
+            type: "input",
+        },
+        {
+            name: "Add",
+            message: "What would you like to do?",
+            type: "input",
+        },
+        {
+            name: "firstname",
+            message: "What is the employee's first name?",
+            type: "input",
+        },
+        {
+            name: "lastname",
+            message: "What is the employees lastname?",
+            type: "input",
+        },
+        {
+            name: "labeling",
+            message: "What is the employee's role?",
+            type: "input",
+        },
+        {
+            name: "Service",
+            message: "Who is the employees manager?",
+            type: "input",
+        },
+        {
+            name: "Update",
+            message: "What would you like to do?",
+            type: "input",
+        },
+        {
+            name: "role",
+            message: "Which employee's role do you want to update?",
+            type: "input",
+        },
+    ])
+    .then((answers) => {
+        // Use user feedback for... whatever!!
+        // {{input}}
+        console.log(answers);
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else went wrong
+        }
+    });
