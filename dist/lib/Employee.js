e543a20369a2d0374ddf7f3f02bd1467f0acfe4d
Crystal Soto-Estrada <Csoto.lacombe@gmail.com>
2021-12-03 19:25:25 -0600

    added questions in for lib folder

--- dist/lib/Employee.js
+++ dist/lib/Employee.js
@@ -1,7 +1,31 @@
 var inquirer = require("inquirer");
 inquirer
   .prompt([
-    /* Pass your questions in here */
+    {
+      name: "name",
+      message: " What is Member's name?",
+      type: "input",
+    },
+    {
+      name: "info",
+      message: "Summary bio on Member",
+      type: "input",
+    },
+    {
+      name: "id",
+      message: "What would you like to do?",
+      type: "input",
+    },
+    {
+      name: "role",
+      message: "Which employee's role do you want to update?",
+      type: "input",
+    },
+    {
+      name: "github",
+      message: "What is your github username?",
+      type: "input",
+    },
   ])
   .then((answers) => {
     // Use user feedback for... whatever!!
--- dist/lib/Intern.js
+++ dist/lib/Intern.js
@@ -1,7 +1,36 @@
 var inquirer = require("inquirer");
 inquirer
   .prompt([
-    /* Pass your questions in here */
+    {
+      name: "name",
+      message: " What is Member's name?",
+      type: "input",
+    },
+    {
+      name: "info",
+      message: "Summary bio on Member",
+      type: "input",
+    },
+    {
+      name: "id",
+      message: "What would you like to do?",
+      type: "input",
+    },
+    {
+      name: "role",
+      message: "Which employee's role do you want to update?",
+      type: "input",
+    },
+    {
+      name: "github",
+      message: "What is your github username?",
+      type: "input",
+    },
+    {
+      name: "school",
+      message: "What is the school did you attend?",
+      type: "input",
+    },
   ])
   .then((answers) => {
     // Use user feedback for... whatever!!
